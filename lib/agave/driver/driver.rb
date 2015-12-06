1614cbdbd1923f12eb1ccea31149cdfbda998e6d
Aaron Vogt <atvogt@localhost.localdomain>
2015-12-05 21:05:01 -0500

    Fixed apapter config inheritance issue
    Class method clear for resetting the adapter hash.  Allows each connection instance to take on it's own config.

--- lib/agave/driver/driver.rb
+++ lib/agave/driver/driver.rb
@@ -1,44 +1,56 @@
 module Agave
    module Adapter
-      @config =
-       {
-        :driver => nil,
-        :host => nil,
-        :database => nil,
-        :username => nil,
-        :password => nil,
-        :charset => 'utf8'
-      }
 
-      def self.option(key, val)
-        begin
-           @config.each do |k,v|
-             unless Error::InvalidDriverKey.check_otpion? key, @config
-                @config[key] = val
-             end
-           end
-        rescue Agave::Error::Standard => e
-           puts e.message key, @config
-        end
-      end # METHOD: option
+   @config =
+    {
+     :driver => nil,
+     :host => nil,
+     :database => nil,
+     :username => nil,
+     :password => nil,
+     :prefix => nil,
+     :charset => 'utf8'
+   }
 
-     def self.config
-       @config
-     end
+
+   def self.set(key, val)
+      begin
+        @config[key] = val << "_" if key == :prefix
+         @config.each do |k,v|
+            unless Error::DriverKey.check? key, @config
+               @config[key] = val
+            end
+         end
+      rescue Agave::Error::Standard => e
+         puts e.message key, @config
+      end
+   end # METHOD: option
+
+   def self.clear
+      @config.map do |k,v|
+         @config[k] = k == :charset ? 'utf8' : nil
+      end
+   end
+
+   def self.config
+    @config
+   end
 
 
   end # MODULE: Adapter
 
 
    class SQLite
-     include Adapter
+      include Adapter
+      @config
 
       class << self
 
-         def connect(db)
-            Agave::Adapter.option :drver, 'sqlite'
-            Agave::Adapter.option :database, db
-            conn = Agave::Connection.new Agave::Adapter.config
+         def connect(&block)
+            Adapter.set :driver, 'sqlite'
+            block.call(Agave::Adapter)
+            conn = Agave::Connection.new Adapter.config
+            Adapter.clear
          end
 
       end # CLASS : self
@@ -46,12 +58,36 @@ module Agave
    end # CLASS : SQLite
 
 
-
    class MySQL
-   end
+      include Adapter
+      @config
 
-   class PG
+      class << self
 
-   end
+         def connect(&block)
+            Adapter.set :driver, 'mysql'
+            block.call(Agave::Adapter)
+            conn = Agave::Connection.new Adapter.config
+            Adapter.clear
+         end
 
-end
+      end # CLASS:self
+   end # CLASS:MySQL
+
+   class PostgreSQL
+      include Adapter
+      @config
+
+      class << self
+
+         def connect(&block)
+            Adapter.set :driver, 'postgre'
+            block.call(Agave::Adapter)
+            conn = Agave::Connection.new Adapter.config
+            Adapter.clear
+         end
+
+      end # CLASS:self
+   end # CLASS:PostreSQL
+
+end # MODULE:Agave
