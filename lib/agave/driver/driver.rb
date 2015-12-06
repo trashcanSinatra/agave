3362871205e037eae038b3b4bf41d304ccd71078
Aaron Vogt <atvogt@iupui.edu>
2015-12-06 16:30:16 -0500

    Refactored driver.rb
    Added AdapterConnection to parent the three sub classes MySQL, SQLite, PostgreSQL. Rolled connect method into one class.  Added set_driver method to Adapter module.

--- lib/agave/driver/driver.rb
+++ lib/agave/driver/driver.rb
@@ -1,93 +1,81 @@
 module Agave
    module Adapter
 
-   @config =
-    {
-     :driver => nil,
-     :host => nil,
-     :database => nil,
-     :username => nil,
-     :password => nil,
-     :prefix => nil,
-     :charset => 'utf8'
-   }
+      @config =
+       {
+        :driver => nil,
+        :host => nil,
+        :database => nil,
+        :username => nil,
+        :password => nil,
+        :prefix => nil,
+        :charset => 'utf8'
+      }
 
 
-   def self.set(key, val)
-      begin
-        @config[key] = val << "_" if key == :prefix
-         @config.each do |k,v|
-            unless Error::DriverKey.check? key, @config
-               @config[key] = val
+      def self.set(key, val)
+         begin
+           @config[key] = val << "_" if key == :prefix
+            @config.each do |k,v|
+               unless Error::DriverKey.check? key, @config
+                  @config[key] = val
+               end
             end
+         rescue Agave::Error::Standard => e
+            puts e.message key, @config
          end
-      rescue Agave::Error::Standard => e
-         puts e.message key, @config
+      end # METHOD: option
+
+
+      private
+
+      def self.set_driver(cls)
+         case cls
+         when 'Agave::SQLite'
+            Adapter.set :driver, 'sqlite'
+         when 'Agave::MySQL'
+            Adapter.set :driver, 'mysql'
+         when 'Agave::PostgreSQL'
+            Adapter.set :driver, 'pgsql'
+         else
+            Adapter.set :driver, 'sqlite'
+         end
       end
-   end # METHOD: option
 
-   def self.clear
-      @config.map do |k,v|
-         @config[k] = k == :charset ? 'utf8' : nil
+
+      def self.clear
+         @config.map do |k,v|
+            @config[k] = k == :charset ? 'utf8' : nil
+         end
       end
-   end
 
-   def self.config
-    @config
-   end
 
+      def self.config
+       @config
+      end
 
   end # MODULE: Adapter
 
+  class AdapterConnection
+     include Adapter
 
-   class SQLite
-      include Adapter
-      @config
+     class << self
 
-      class << self
+        def connect(&block)
+           Adapter.set_driver(self.to_s)
+           block.call(Agave::Adapter)
+           conn = Agave::Connection.new Adapter.config
+           Adapter.clear
+        end
 
-         def connect(&block)
-            Adapter.set :driver, 'sqlite'
-            block.call(Agave::Adapter)
-            conn = Agave::Connection.new Adapter.config
-            Adapter.clear
-         end
+     end #CLASS : self
 
-      end # CLASS : self
+  end
 
-   end # CLASS : SQLite
+   class SQLite < AdapterConnection; end #CLASS : SQLite
 
+   class MySQL < AdapterConnection; end #CLASS: MySQL
 
-   class MySQL
-      include Adapter
-      @config
-
-      class << self
-
-         def connect(&block)
-            Adapter.set :driver, 'mysql'
-            block.call(Agave::Adapter)
-            conn = Agave::Connection.new Adapter.config
-            Adapter.clear
-         end
-
-      end # CLASS:self
-   end # CLASS:MySQL
-
-   class PostgreSQL
-      include Adapter
-      @config
-
-      class << self
-
-         def connect(&block)
-            Adapter.set :driver, 'postgre'
-            block.call(Agave::Adapter)
-            conn = Agave::Connection.new Adapter.config
-            Adapter.clear
-         end
-
-      end # CLASS:self
-   end # CLASS:PostreSQL
+   class PostgreSQL < AdapterConnection; end #CLASS: PostreSQL
 
 end # MODULE:Agave
