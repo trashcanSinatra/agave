a47f834a30d60d23d7c9949b45e65a526535dfcf
Aaron Vogt <atvogt@iupui.edu>
2015-12-27 23:25:02 -0500

    Passing Connection Object to Query Builder Class
    Done with testing passing the config hash.

--- lib/agave/driver/connection.rb
+++ lib/agave/driver/connection.rb
@@ -9,13 +9,14 @@ module Agave
          when 'sqlite'
             puts @database
             conn = SQLite3::Database.open "#{@database}"
-            Agave::Query.make config
+            Agave::Query.make conn
          when 'mysql'
             conn = Mysql2::Client.new(:host => @host,
                                       :database => @database,
                                       :username => @username,
-                                      :password => @password)
-            Agave::Query.make config
+                                      :password => @password
+                                      )
+            Agave::Query.make conn
          when 'pgsql'
             Agave::Query.make config
          end
